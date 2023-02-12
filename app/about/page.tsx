6bf56d0a5f712ec2af6716a3e7d69ae01e439f5b
alisher714 <alishermalik1100@gmail.com>
2023-02-12 11:19:06 +0500

    home to about, about to home

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function About() {
+    return (
+        <>
+            <h1>
+                About page
+            </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio voluptatum inventore voluptate tempore ut praesentium, obcaecati voluptas corrupti ullam soluta, animi in esse consectetur dolores voluptates eveniet hic fugit non ipsa excepturi ad repudiandae.</p>
+            <Link href="/">Home</Link>
+        </>
+    )
+}
